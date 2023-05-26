2f576caf43999c4f0ebdbd8335a9813403d725e8
dlehdduq22 <dlehdduq22@naver.com>
2023-05-26 18:44:25 +0900

    do it html chapter 15 자바스크립트 이벤트, 등등 공부

---
+++ web-basic/src/15/js/function2.js
@@ -0,0 +1,27 @@
+let add2 = function(x, y) {
+    return x + y;
+}
+
+
+main();
+
+function add(x, y){
+    return x + y;
+}
+
+let test = 10;
+
+function main(){
+    let num1 = 10;
+    let num2 = 20;
+    
+   let result = add(num1, num2);
+    console.log("결과: " +  result);
+    
+    result = add2(num1, num2);
+    console.log("결과2: " + result);
+
+    result = function(x, y) {return x + y}(num1, num2);
+    console.log("결과3: " + result);
+}
+
